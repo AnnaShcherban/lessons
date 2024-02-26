63a16851fb5e788161caaf0f865610aa263b1976
AnnaShcherban <160736945+AnnaShcherban@users.noreply.github.com>
2024-02-26 13:35:53 +0200

    Add files via upload

---
+++ WEB/js/script.js
@@ -0,0 +1,11 @@
+//Знайти кількість років
+
+//Описуємо і вводимо значення зміних
+let happyBirthay = parseFloat(prompt("Введіть дату свого народження", "0"))
+const CURRENR_YEAR = 2024
+
+//Знаходимо результат
+let year_Birth = CURRENR_YEAR - happyBirthay
+
+//Виводимо результат
+document.write(`Кількість років=${year_Birth}`)
